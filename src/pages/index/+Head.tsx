b966dc8d39ff463dc9d413bfac117db2b66abcf5
Alex Goodkind <alex@goodkind.io>
2025-08-15 18:56:03 +0200

    Enhance meta data handling in Head component and update tracking scripts
    - Added new properties (title, ogImage, url, favicon, manifest, appleTouchIcon) to the calculateMeta function.
    - Refactored Head component to utilize the new meta properties for improved SEO and social sharing.
    - Updated Google Tag Manager and Facebook Pixel initialization to use destructured values from calculateTracking.

--- src/pages/index/+Head.tsx
+++ src/pages/index/+Head.tsx
@@ -1,17 +1,19 @@
 import { calculateMeta, calculateTracking } from "@/calculate-data";
 
 const gtagScript = () => {
+  const { gtagId } = calculateTracking();
   return `
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       dataLayer.push(arguments);
     }
     gtag("js", new Date());
-    gtag("config", "${calculateTracking().gtagId}");
+    gtag("config", "${gtagId}");
   `;
 };
 
 const fbqScript = () => {
+  const { fbPixelId } = calculateTracking();
   return `
     !(function (f, b, e, v, n, t, s) {
       if (f.fbq) return;
@@ -34,41 +36,57 @@ const fbqScript = () => {
       "script",
       "https://connect.facebook.net/en_US/fbevents.js",
     );
-    fbq("init", "${calculateTracking().fbPixelId}");
+    fbq("init", "${fbPixelId}");
     fbq("track", "PageView");
   `;
 };
 
 export default function Head() {
+  const { favicon, manifest, title, description, url, ogImage } =
+    calculateMeta();
+
+  const { gtagId, fbDomainVerification } = calculateTracking();
   return (
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content={calculateMeta().description} />
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
+      <meta name="description" content={description} />
+      <link rel="icon" href={favicon} sizes="any" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href={favicon} />
+      <link rel="manifest" href={manifest} />
+
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImage} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:domain"
+        content={url ? new URL(url!).hostname : undefined}
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImage} />
+
+      {/* Facebook */}
       <meta
         name="facebook-domain-verification"
-        content={calculateTracking().fbDomainVerification}
+        content={fbDomainVerification}
       />
 
+      {/* Google Tag Manager */}
       <script
         async
         defer
-        src={`https://www.googletagmanager.com/gtag/js?id=${calculateTracking().gtagId}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtagId}`}
       ></script>
       <script>{gtagScript()}</script>
       <script>{fbqScript()}</script>
